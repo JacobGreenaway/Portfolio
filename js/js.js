cac91381bba61c55dcb5126edee22202694d13a6
JacobGreenaway <jacobgreenaway12@gmail.com>
2015-06-11 03:42:11 +1000

    Added plugins and some content
    jquery easing
    plusanchor
    headroom
    fade in animation

---
+++ js/js.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+	//alert( $(".please"));
+    $(".myClass").hide().fadeIn(1000);
+
+    $('body').plusAnchor({
+		easing: 'easeOutExpo',
+		speed:  1000
+	});
+
+	/* Hides the nav bar on scrolling */
+
+	$(function () {
+	  	$('.navbar').each(function () {
+	    	var $doc = $(document)
+	      		, $win = $(window)
+	      		, $self = $(this)
+
+	      		, isHidden = false
+	      		, lastScrollTop = 0
+
+			$win.on('scroll', function () {
+			    var scrollTop = $doc.scrollTop()
+			    var offset = scrollTop - lastScrollTop
+			    lastScrollTop = scrollTop
+
+			    // min-offset, min-scroll-top
+			    if (offset > 10 && scrollTop > 200 ) {
+				    if (!isHidden) {
+				        $self.addClass('headroom-hidden')
+				          	isHidden = true
+				        }
+			    } else if (offset < -10 || scrollTop < 200) {
+				    if (isHidden) {
+				        $self.removeClass('headroom-hidden')
+				        isHidden = false
+				    }
+	      		}
+	    	})
+	  	})
+	})
+	 
+});
+
+
